0fffebe46611d931533f9772e41c35076eeffc52
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-03-24 21:18:57 -0400

    Added code to push tables into a dictionary

--- eye_gaze.py
+++ eye_gaze.py
@@ -1,12 +1,18 @@
 import csv
 import sys
 
+tables = dict()
+index = 0
+
 if len(sys.argv) > 1:
     path = sys.argv[1]
     with open(path) as data:
         csv_reader = csv.reader(data, delimiter="\t")
+        tables[f'table_{index}'] = []
         for row in csv_reader:
-            if not "".join(row):  # identies empty rows
-                print("hi")
+            if not "".join(row):  # identifies empty rows
+                index += 1
+                tables[f'table_{index}'] = []
+            tables[f'table_{index}'].append(row)
 else:
     print("Please provide the path to a data file.")
