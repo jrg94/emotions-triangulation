c9296902347ba67ab963bf08280f21b0a0d34adb
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-03-24 21:30:37 -0400

    Added table labels

--- eye_gaze.py
+++ eye_gaze.py
@@ -16,3 +16,8 @@ if len(sys.argv) > 1:
             tables[f'table_{index}'].append(row)
 else:
     print("Please provide the path to a data file.")
+
+META_DATA = "table_0"
+GAZE_CALIBRATION_POINTS_DETAILS = "table_1"
+GAZE_CALIBRATION_SUMMARY_DETAILS = "table_2"
+DATA = "table_3"
