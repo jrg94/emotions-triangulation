b170f7fac5a0453340364ab6917a1003c5fcc91c
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-03-26 17:28:24 -0400

    Added calculation for fixation sequence length

--- eye_gaze.py
+++ eye_gaze.py
@@ -1,6 +1,15 @@
 import csv
 import sys
+
 import pandas as pd
+
+META_DATA = "table_0"
+GAZE_CALIBRATION_POINTS_DETAILS = "table_1"
+GAZE_CALIBRATION_SUMMARY_DETAILS = "table_2"
+DATA = "table_3"
+STIMULUS_NAME = "StimulusName"
+FIXATION_DURATION = "FixationDuration"
+FIXATION_SEQUENCE = "FixationSeq"
 
 tables = dict()
 index = 0
@@ -19,12 +28,11 @@ if len(sys.argv) > 1:
 else:
     print("Please provide the path to a data file.")
 
-META_DATA = "table_0"
-GAZE_CALIBRATION_POINTS_DETAILS = "table_1"
-GAZE_CALIBRATION_SUMMARY_DETAILS = "table_2"
-DATA = "table_3"
-
 data_table = tables[DATA]
 header = data_table[0]
 df = pd.DataFrame(data_table[1:], columns=header)
-
+stimuli = df[STIMULUS_NAME].unique()
+for stimulus in stimuli:
+    stimulus_filter = df[STIMULUS_NAME] == stimulus
+    fixation_sequence_length = df[stimulus_filter][FIXATION_SEQUENCE].max()
+    print(f'{stimulus}: {fixation_sequence_length}')
