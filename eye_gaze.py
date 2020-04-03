32753a1ee58c4a41c049ea683543fd35a1f5826d
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-04-02 23:25:46 -0400

    Added plotting

--- eye_gaze.py
+++ eye_gaze.py
@@ -1,8 +1,10 @@
 import csv
 import sys
-from datetime import datetime
 
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
 
 META_DATA = "table_0"
 GAZE_CALIBRATION_POINTS_DETAILS = "table_1"
@@ -13,6 +15,7 @@ FIXATION_DURATION = "FixationDuration"
 FIXATION_SEQUENCE = "FixationSeq"
 TIMESTAMP = "Timestamp"
 TIME_FORMAT = "%Y%m%d_%H%M%S%f"
+WINDOW = "30S"
 
 
 def main():
@@ -20,7 +23,7 @@ def main():
         paths = sys.argv[1:]
         participants = read_tsv_files(*paths)
         for participant in paths:
-            output_statistics(participants[participant])
+            generate_statistics(participants[participant])
 
 
 def read_tsv_files(*paths) -> dict:
@@ -75,17 +78,102 @@ def clean_data(tables: dict) -> pd.DataFrame:
     return data
 
 
-def statistics_windows(stimulus_data: pd.DataFrame, duration: int = None) -> pd.DataFrame:
-    if duration:
-        timestamps = pd.to_datetime(stimulus_data[TIMESTAMP], format=TIME_FORMAT)
-        return timestamps
-    else:
-        return stimulus_data
-
-
-def output_statistics(tables: dict):
+def summary_report(stimulus: str, stimulus_data: pd.DataFrame) -> dict:
     """
-    Outputs summary statistics for a participant.
+    Generates a summary report of the data for a given stimuli.
+
+    :param stimulus: a stimulus like MATLAB
+    :param stimulus_data: the section of the data only relevant to the stimulus
+    :return: a summary report as a dictionary
+    """
+    fixation_sequence_sans_dupes = stimulus_data.drop_duplicates(FIXATION_SEQUENCE)
+    fixation_sequence: pd.Series = fixation_sequence_sans_dupes[FIXATION_SEQUENCE]
+    fixation_duration: pd.Series = fixation_sequence_sans_dupes[FIXATION_DURATION]
+    fixation_sequence_length = fixation_sequence.max()
+    fixation_sequence_duration = fixation_duration.sum()
+    seconds = fixation_sequence_duration / 1000
+    fixation_duration_mean = fixation_duration.mean()
+    fixation_duration_median = fixation_duration.median()
+    fixation_duration_min = fixation_duration.min()
+    fixation_duration_max = fixation_duration.max()
+    fixation_duration_std = fixation_duration.std()
+    start_date_time = stimulus_data.iloc[0][TIMESTAMP]
+    end_date_time = stimulus_data.iloc[-1][TIMESTAMP]
+    return {
+        f"{stimulus}": {
+            "Stimulus Metrics": {
+                "Start time": start_date_time,
+                "End time": end_date_time,
+                "Duration": end_date_time - start_date_time
+            },
+            "Fixation Sequence Metrics": {
+                "Number of Points": fixation_sequence_length,
+                "Duration (seconds)": seconds
+            },
+            "Fixation Duration Metrics": {
+                "Mean (ms)": fixation_duration_mean,
+                "Median (ms)": fixation_duration_median,
+                "Standard Deviation (ms)": fixation_duration_std,
+                "Minimum (ms)": fixation_duration_min,
+                "Maximum (ms)": fixation_duration_max
+            }
+        }
+    }
+
+
+def windowed_metrics(stimulus_data: pd.DataFrame) -> tuple:
+    """
+    Computes fixation counts and average fixation duration within some window of time.
+
+    :param stimulus_data: the section of the data only relevant to the stimulus
+    :return: fixation counts, average fixation duration (tuple)
+    """
+    fixation_sequence_sans_dupes = stimulus_data.drop_duplicates(FIXATION_SEQUENCE)
+    windowed_data = fixation_sequence_sans_dupes.resample(WINDOW, on=TIMESTAMP)
+    unique_fixation_counts = windowed_data.nunique()[FIXATION_SEQUENCE]
+    average_fixation_duration = windowed_data.mean()[FIXATION_DURATION]
+    return unique_fixation_counts, average_fixation_duration
+
+
+def output_summary_report(metrics: dict, depth: int = 0):
+    """
+    Dumps a summary report as a string.
+
+    :param metrics: a dictionary of metrics
+    :param depth: the depth of indentation
+    :return: None
+    """
+    for k, v in metrics.items():
+        indent = "\t" * depth
+        if isinstance(v, dict):
+            print(f'{indent}{k}')
+            output_summary_report(v, depth + 1)
+        else:
+            print(f'{indent}{k}: {v}')
+
+
+def plot_data(fixation_counts, avg_fixation_duration):
+    fig, ax = plt.subplots()
+
+    color = 'tab:red'
+    ax.plot(fixation_counts, color=color)
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Fixation Count", color=color)
+    ax.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax.twinx()
+
+    color = 'tab:blue'
+    ax2.plot(avg_fixation_duration, color=color)
+    ax2.set_ylabel("Mean Fixation Duration", color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    plt.show()
+
+
+def generate_statistics(tables: dict):
+    """
+    Generates various statistics for a participant.
 
     :param tables: a participant table
     :return: None
@@ -95,39 +183,10 @@ def output_statistics(tables: dict):
     for stimulus in stimuli:
         stimulus_filter = df[STIMULUS_NAME] == stimulus
         stimulus_data = df[stimulus_filter]
-        fixation_sequence_sans_dupes = stimulus_data.drop_duplicates(FIXATION_SEQUENCE)
-        fixation_sequence: pd.Series = fixation_sequence_sans_dupes[FIXATION_SEQUENCE]
-        fixation_duration: pd.Series = fixation_sequence_sans_dupes[FIXATION_DURATION]
-        fixation_sequence_length = fixation_sequence.max()
-        fixation_sequence_duration = fixation_duration.sum()
-        seconds = fixation_sequence_duration / 1000
-        fixation_duration_mean = fixation_duration.mean()
-        fixation_duration_median = fixation_duration.median()
-        fixation_duration_min = fixation_duration.min()
-        fixation_duration_max = fixation_duration.max()
-        fixation_duration_std = fixation_duration.std()
-        start_date_time = stimulus_data.iloc[0][TIMESTAMP]
-        end_date_time = stimulus_data.iloc[-1][TIMESTAMP]
-        print(
-            "\n".join(
-                [
-                    f'{stimulus}:',
-                    f'\t Stimulus Metrics:',
-                    f'\t\tStart time: {start_date_time}',
-                    f'\t\tEnd time: {end_date_time}',
-                    f'\t\tDuration: {end_date_time - start_date_time}',
-                    f'\tFixation Sequence Metrics:',
-                    f'\t\tNumber of Points: {fixation_sequence_length} points',
-                    f'\t\tDuration: {seconds} seconds',
-                    f'\tFixation Duration Metrics:',
-                    f'\t\tMean: {fixation_duration_mean} milliseconds',
-                    f'\t\tMedian: {fixation_duration_median} milliseconds',
-                    f'\t\tStandard Deviation: {fixation_duration_std} milliseconds',
-                    f'\t\tMinimum: {fixation_duration_min} milliseconds',
-                    f'\t\tMaximum: {fixation_duration_max} milliseconds',
-                ]
-            )
-        )
+        report = summary_report(stimulus, stimulus_data)
+        output_summary_report(report)
+        fixation_counts, avg_fixation_duration = windowed_metrics(stimulus_data)
+        plot_data(fixation_counts, avg_fixation_duration)
 
 
 if __name__ == '__main__':
